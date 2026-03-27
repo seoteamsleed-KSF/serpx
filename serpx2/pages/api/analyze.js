40023f575e27637970521277a1f09add4f7c6ee2
seoteamsleed-KSF <seoteamsleed@gmail.com>
2026-03-27 15:36:39 +0200

    Update analyze.js

--- serpx2/pages/api/analyze.js
+++ serpx2/pages/api/analyze.js
@@ -1,4 +1,5 @@
 import { getAhrefsData } from '../../lib/ahrefs'
+import { getSimilarwebData } from '../../lib/similarweb'
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
